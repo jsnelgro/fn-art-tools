08c53fd9a94e78057a72c54e9edc35f7e1ab9f04
John Snelgrove <johnny.snelgrove@gmail.com>
2018-01-09 15:20:33 -0800

    fix Rect component

--- packages/Rect/src/index.js
+++ packages/Rect/src/index.js
@@ -1,20 +1,25 @@
-// TODO: rewrite after rewriting Shape class...
-
 import React, { Component } from 'react'
 import Shape from '@vzy/shape'
 
 const Rect = (props) => {
-  const { children, ...other } = props
-  let _x, _y;
-  if (!props.origin) {
-    _x = props.width / 2
-    _y = props.height / 2
-  }
+  let {
+    x, y, rot, origin, skew, h = 10, w = 10,
+    children, hasOwnCoordinateSpace = false, ...otherprops
+  } = props
+
   return (
-  <Shape {...other} width={props.width} height={props.height}>
-    <rect x={_x || 0} y={_y || 0} height={100} width={100} />
-    {children}
-  </Shape>)
+    <Shape {...{ x, y, rot, origin, skew,
+      h: hasOwnCoordinateSpace ? h : null,
+      w: hasOwnCoordinateSpace ? w : null
+    }}>
+      <rect
+        height={hasOwnCoordinateSpace ? 100 : h}
+        width={hasOwnCoordinateSpace ? 100 : w}
+        {...otherprops}
+      />
+      {children}
+    </Shape>
+  )
 }
 
 export default Rect
