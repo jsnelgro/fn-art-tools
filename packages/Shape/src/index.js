2d22f8d5094b4777c028f0997453ffe16e007a67
John Snelgrove <johnny.snelgrove@gmail.com>
2018-01-09 10:06:44 -0800

    rewrite shape class

--- packages/Shape/src/index.js
+++ packages/Shape/src/index.js
@@ -1,59 +1,101 @@
-// TODO: Rewrite. Shape class is all sorts of fucked up...
-
 import React, { Component } from 'react'
 import Proptypes from 'prop-types'
+import styled, { css } from 'react-emotion'
+import { withProps } from 'recompose'
+import Canvas from '@vzy/canvas'
 
-class Shape extends Component {
-  static propTypes = {
-    children: Proptypes.node,
-    x: Proptypes.number,
-    y: Proptypes.number,
-    rot: Proptypes.number,
-    origin: Proptypes.oneOfType([Proptypes.array, Proptypes.string]),
-    scale: Proptypes.oneOfType([Proptypes.number, Proptypes.array]),
-    skew: Proptypes.oneOfType([Proptypes.number, Proptypes.array]),
-    transforms: Proptypes.arrayOf(Proptypes.string),
-    points: Proptypes.arrayOf(Proptypes.string)
-  }
+// const withTransformStyle = ({x = 0, y = 0, rot = 0, origin = 'center'}) => css`
+//   transform-origin: ${origin};
+//   transform: ${[
+//     `translate(${x}px, ${y}px)`,
+//     `rotate(${rot}deg)`,
+//   ].join(' ')}
+// `
 
-  getBBox = () => {
-    if (!this.r) return { width: 0, height: 0, x: 0, y: 0 }
-    return this.r.getBBox()
-  }
 
-  render() {
-    let { children, x, y, rot, scale, origin, skew, transforms, path, ...p } = this.props
-    let bbox = this.getBBox()
-    const { height, width } = bbox
-    if (typeof scale === 'number') { scale = [scale, scale] }
-    if (typeof skew === 'number') { skew = [skew, skew] }
-    if (origin === 'center') { origin = [height / 2, width / 2] }
-    origin[0] *= scale[0]
-    origin[1] *= scale[1]
-    x -= origin[0]
-    y -= origin[1]
-    const transform = [
-      ...transforms,
+const Shape = (props) => {
+  let {x, y, rot = 0, origin = 0, ...otherprops} = props
+  let gProps = {
+    transform: [
       `translate(${x}, ${y})`,
-      `rotate(${rot}, ${origin[0]}, ${origin[1]})`,
-      `scale(${scale[0]},${scale[1]})`,
-    ].join(' ')
-    return (<g ref={r => (this.r = r)} transform={transform} {...p}>
-      {path ? <path d={path.join(' ')}></path> : null}
-      {children}
-    </g>)
+      `rotate(${rot})`,
+    ].join(' '),
+    style: { transformOrigin: origin, ...props.style }
+  }
+  let svgProps = {
+    height: props.h,
+    width: props.w,
+    preserveAspectRatio: 'none',
+    overflow: 'visible',
+    viewBox: `0 0 ${100} ${100}`,
+  }
+  return (props.h && props.w) ?
+    (<g {...gProps}><svg {...svgProps} {...otherprops} /></g>) :
+    (<g {...gProps} {...otherprops} />)
+}
+
+export function withTransform () {
+  return (WrappedComponent) => {
+    return ({children, x, y, rot, origin, h = null, w = null, ...props}) => (
+      <Shape x={x} y={y} rot={rot} origin={origin} h={h} w={w}>
+        <WrappedComponent {...props} />
+        {/* <g>{children}</g> */}
+      </Shape>
+    )
   }
 }
 
-Shape.defaultProps = {
-  x: 0,
-  y: 0,
-  rot: 0,
-  scale: [1, 1],
-  skew: 0,
-  origin: 'center',
-  transforms: [],
-  path: []
-}
+// class Shape extends Component {
+//   static propTypes = {
+//     children: Proptypes.node,
+//     x: Proptypes.number,
+//     y: Proptypes.number,
+//     rot: Proptypes.number,
+//     origin: Proptypes.oneOfType([Proptypes.array, Proptypes.string]),
+//     scale: Proptypes.oneOfType([Proptypes.number, Proptypes.array]),
+//     skew: Proptypes.oneOfType([Proptypes.number, Proptypes.array]),
+//     transforms: Proptypes.arrayOf(Proptypes.string),
+//     points: Proptypes.arrayOf(Proptypes.string)
+//   }
+
+//   getBBox = () => {
+//     if (!this.r) return { width: 0, height: 0, x: 0, y: 0 }
+//     return this.r.getBBox()
+//   }
+
+//   render() {
+//     let { children, x, y, rot, scale, origin, skew, transforms, path, ...p } = this.props
+//     let bbox = this.getBBox()
+//     const { height, width } = bbox
+//     if (typeof scale === 'number') { scale = [scale, scale] }
+//     if (typeof skew === 'number') { skew = [skew, skew] }
+//     if (origin === 'center') { origin = [height / 2, width / 2] }
+//     origin[0] *= scale[0]
+//     origin[1] *= scale[1]
+//     x -= origin[0]
+//     y -= origin[1]
+//     const transform = [
+//       ...transforms,
+//       `translate(${x}, ${y})`,
+//       `rotate(${rot}, ${origin[0]}, ${origin[1]})`,
+//       `scale(${scale[0]},${scale[1]})`,
+//     ].join(' ')
+//     return (<g ref={r => (this.r = r)} transform={transform} {...p}>
+//       {path ? <path d={path.join(' ')}></path> : null}
+//       {children}
+//     </g>)
+//   }
+// }
+
+// Shape.defaultProps = {
+//   x: 0,
+//   y: 0,
+//   rot: 0,
+//   scale: [1, 1],
+//   skew: 0,
+//   origin: 'center',
+//   transforms: [],
+//   path: []
+// }
 
 export default Shape
--- packages/Shape/src/shape.story.js
+++ packages/Shape/src/shape.story.js
@@ -1,41 +1,132 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, number, text } from '@storybook/addon-knobs'
+import Canvas from '@vzy/canvas'
+import Shape, { withTransform } from '.'
+
+// import { range } from '@vzy/utils'
 // import readme from './README.md'
 // import { doc } from '/storybook-helpers'
 
-import Canvas from '@vzy/canvas'
-import Shape from '.'
+// const Polygon = withTransform()(({sides, radius}) => {
+//   const angle = 360 / sides
+//   const vertexIndices = range(sides)
+//   const offsetDeg = 90 - ((180 - angle) / 2)
+//   const offset = degreesToRadians(offsetDeg)
+//   const points = pts(sides, radius).map(({ r, theta }) => [
+//     cx + r * Math.cos(theta), 
+//     cy + r * Math.sin(theta),
+//   ])
+//   .join(' ')
+//   return <polygon points={points} />
+// })
+
+// const Rect = withTransform()((...props) => {
+//   return <rect height={100} width={100} {...props} />
+// })
 
 storiesOf('viz/Shape', module)
   .addDecorator(withKnobs)
-  .add(
-    'kitchen sink',
-    () => {
-      const opts = { range: true, min: 0, max: 100 }
-      const x = number('x', 15, opts)
-      const y = number('y', 45, opts)
-      const ox = number('origin x', 25, {...opts, min: -50 })
-      const oy = number('origin y', 25, {...opts, min: -50 })
-      const rot = number('rotate', 75, {...opts, max: 360 })
-      const scale = number('scale', 0.5, {...opts, max: 3, step: 0.01})
-      const skx = number('skew x', 0, {...opts, min: -180, max: 90 })
-      const sky = number('skew y', 0, {...opts, min: -180, max: 90 })
-      const tfms = text('transforms', '')
-      return (
-        <Canvas>
-          <Shape
-            origin={[ox, oy]}
-            x={x} y={y}
-            rot={rot}
-            scale={scale}
-            skew={[skx, sky]}
-            transforms={[tfms]}
-            >
-            <rect height={50} width={50} fill="#4a4a4a" />
-            <rect x={5} height={25} width={25} fill="navajowhite" />
-            <circle cx={ox} cy={oy} r={1/scale} fill="indianred" />
-          </Shape>
-        </Canvas>
-      )
-    })
+  .add('Basic Shape HOC', () => {
+    const opts = { range: true, min: 0, max: 100 }
+    const x = number('x', 15, opts)
+    const y = number('y', 15, opts)
+    const rot = number('rotation', 15, {...opts, max: 360})
+    const origin = text('origin', 'center')
+
+    return (
+    <Canvas>
+      <Shape fill={'cadetblue'} x={x} y={y} rot={rot} origin={origin}>
+        <rect height={25} width={25} />
+      </Shape>
+    </Canvas>
+    )
+  })
+  .add('Compound Shape', () => {
+    const opts = { range: true, min: 0, max: 100 }
+    const x = number('x', 15, opts)
+    const y = number('y', 15, opts)
+    const rot = number('rotation', 15, {...opts, max: 360})
+    const origin = text('origin', 'center')
+
+    return (
+    <Canvas>
+      <Shape x={x} y={y} rot={rot} origin={origin}>
+        <rect height={25} width={25} fill={'cadetblue'} />
+        <circle r={2} cx={0} cy={0} fill={'indianred'} />
+      </Shape>
+    </Canvas>
+    )
+  })
+  .add('Nested Shapes', () => {
+    const opts = { range: true, min: 0, max: 100 }
+    const x = number('x', 36, opts)
+    const y = number('y', 36, opts)
+    const rot = number('rotation', 0, {...opts, max: 360})
+    const origin = text('origin', 'center')
+
+    return (
+    <Canvas style={{width: '100%', height:'97vh'}}>
+      <rect height={100} width={100} fill={'#4a4a4a'}/>
+      <Shape x={x} y={y} rot={rot} origin={origin}>
+        <rect x={0} y={0} height={25} width={25} fill={'navajowhite'}/>
+        <Shape y={Math.min(Math.max((y/3) - 10, -2), 3)} x={Math.min(Math.max((x/3) - 12, -4), 4)}>
+          <rect x={5} y={4} height={8} width={5} fill={'cadetblue'}/>
+          <rect x={15} y={4} height={8} width={5} fill={'cadetblue'}/>
+        </Shape>
+        <rect x={5} y={16} height={5} width={15} fill={'indianred'}/>
+      </Shape>
+    </Canvas>
+    )
+  })
+  .add('Nested Shapes with new coordinate space', () => {
+    const opts = { range: true, min: 0, max: 100 }
+    const x = number('x', 36, opts)
+    const y = number('y', 36, opts)
+    const h = number('height', 25, opts)
+    const w = number('width', 25, opts)
+    const rot = number('rotation', 0, {...opts, max: 360})
+    const origin = text('origin', 'center')
+    return (
+      <Canvas style={{border: '1px solid #4a4a4a', margin: '0 auto', height:'97vh'}}>
+        <Shape h={h} w={w} x={x} y={y} rot={rot} origin={origin}>
+          <rect fill={'salmon'} height={100} width={100} />
+          <circle fill={'thistle'} cx={50} cy={50} r={25} />
+          {/* <Shape></Shape> */}
+        </Shape>
+      </Canvas>
+    )
+  })
+
+// storiesOf('viz/Shape', module)
+//   .addDecorator(withKnobs)
+//   .add(
+//     'kitchen sink',
+//     () => {
+      // const opts = { range: true, min: 0, max: 100 }
+//       const x = number('x', 15, opts)
+//       const y = number('y', 45, opts)
+//       const ox = number('origin x', 25, {...opts, min: -50 })
+//       const oy = number('origin y', 25, {...opts, min: -50 })
+//       const rot = number('rotate', 75, {...opts, max: 360 })
+//       const scale = number('scale', 0.5, {...opts, max: 3, step: 0.01})
+//       const skx = number('skew x', 0, {...opts, min: -180, max: 90 })
+//       const sky = number('skew y', 0, {...opts, min: -180, max: 90 })
+//       const tfms = text('transforms', '')
+//       return (
+//         <Canvas>
+//           <Shape
+//             origin={[ox, oy]}
+//             x={x} y={y}
+//             rot={rot}
+//             scale={scale}
+//             skew={[skx, sky]}
+//             transforms={[tfms]}
+//             >
+//             <rect height={50} width={50} fill="#4a4a4a" />
+//             <rect x={5} height={25} width={25} fill="navajowhite" />
+//             <circle cx={ox} cy={oy} r={1/scale} fill="indianred" />
+//           </Shape>
+//         </Canvas>
+//       )
+//     })
