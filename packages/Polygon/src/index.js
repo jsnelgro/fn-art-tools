60aeea7fef875c8bca1273d98d2dad03546fc1ff
John Snelgrove <johnny.snelgrove@gmail.com>
2018-01-12 16:47:59 -0800

    basic polygon component

--- packages/Polygon/src/index.js
+++ packages/Polygon/src/index.js
@@ -1,13 +1,26 @@
+import React from 'react'
+import Shape from '@vzy/shape'
+import Path, { pathHelpers } from '@vzy/path'
+const { d, moveTo, lineTo, closePath } = pathHelpers
 
-import React, { Component } from 'react'
+const Polygon = (props) => {
+  let { x, y, w = 10, h = 10, sides = 6, ...other } = props
+  let points = [...Array(sides + 1).keys()].map((i) => {
+    const angle = Math.PI / 2 + 2 * Math.PI * i / sides
+    return [
+      (w / 2) * Math.cos(angle),
+      (h / 2) * Math.sin(angle)
+    ]
+  })
 
-class Polygon extends Component {
-
-  render () {
-    const { txt } = this.props
-    return (<div>{txt}</div>)
-  }
-
+  const path = d([
+    moveTo(points[0][0], points[0][1]),
+    ...points.map(p => lineTo(...p)),
+    closePath()
+  ])
+  return (
+    <Path d={path} {...props} />
+  )
 }
 
 export default Polygon
--- packages/Polygon/src/polygon.story.js
+++ packages/Polygon/src/polygon.story.js
@@ -1,16 +1,35 @@
-
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs, text } from '@storybook/addon-knobs'
+import { withKnobs, text, number } from '@storybook/addon-knobs'
 
+import { Canvas } from '@vzy/playground'
 import Polygon from '.'
 
-storiesOf('Polygon', module)
+storiesOf('viz/Polygon', module)
   .addDecorator(withKnobs)
-  .add('example 1', () => {
-    const txt = text('inner txt', 'I am a Polygon component!')
+  .add('simple polygon', () => {
+    let opts = { min: 0, max: 100 }
+    const sides = number('sides', 6, opts)
+    const h = number('height', 50, opts)
+    const w = number('width', 50, opts)
+    const x = number('x', 50, opts)
+    const y = number('y', 50, opts)
+    const rot = number('rotate', 0, {...opts, max: 360})
+    const origin = text('origin', 'center')
+
     return (
-      <Polygon txt={txt}>
-      </Polygon>
+      <Canvas style={{height: '96vh', background:'moccasin'}}>
+        <Polygon {...{
+          sides, h, w, x, y, rot, origin,
+          fill: 'indianred',
+          stroke: '#4a4a4a'
+        }}>
+          <Polygon {...{
+            x:0, y:0, h: h/3, w: w/3,
+            fill: 'moccasin', stroke: '#4a4a4a',
+            sides: Math.max(sides - 1, 2)
+          }}/>
+        </Polygon>
+      </Canvas>
     )
   })
